a203bc82aed1c1ec759c9f322db253cc28452743
Trevor Sinnott <sinnott.trevor@gmail.com>
2018-09-24 14:41:08 +0900

    first commit

---
+++ spec/hangman_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Hangman do
+  it "has a version number" do
+    expect(Hangman::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end

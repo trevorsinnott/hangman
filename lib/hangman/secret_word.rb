4c89dc195e8e0cc29d13e801f382d896a0d8e9e4
Trevor Sinnott <sinnott.trevor@gmail.com>
2018-09-25 08:55:52 +0900

    add length_ok? method and check that random_word is correct length. Make sure scret_word is lowercase.

--- lib/hangman/secret_word.rb
+++ lib/hangman/secret_word.rb
@@ -2,11 +2,26 @@ module Hangman
   class SecretWord
     attr_reader :word
     def initialize(word = random_word)
-      @word = word
+      @word = word.downcase
     end
 
+    private
+
     def random_word
-      File.readlines("./lib/5desk.txt").sample
+      selection = ""
+      while selection == ""
+        temporary = File.readlines("./lib/5desk.txt").sample.strip
+        selection = temporary if length_ok?(temporary)
+      end
+      selection
+    end
+
+    def length_ok?(check)
+      if (check.length > 4) && (check.length < 13)
+        return true
+      else
+        return false
+      end
     end
   end
 end
--- spec/secret_word_spec.rb
+++ spec/secret_word_spec.rb
@@ -8,13 +8,32 @@ module Hangman
         secret_word = SecretWord.new(word)
         expect(secret_word.word).to eq "some word"
       end
-    end
 
-    context "#random_word" do
       it "assigns a string to the @word variable if no string is initially given" do
         secret_word = SecretWord.new
         word_string = secret_word.word.to_s
         expect(secret_word.word).to eq word_string
+      end
+
+      it "randomly selects a word that is more than 4 characters" do
+        secret_word = SecretWord.new
+        expect(secret_word.word.length).to be > 4
+      end
+
+      it "randomly selects a word that is less than 13 characters" do
+        secret_word = SecretWord.new
+        expect(secret_word.word.length).to be < 13
+      end
+
+      it "converts custom string to lower-case" do
+        secret_word = SecretWord.new("TEsT")
+        expect(secret_word.word).to eq "test"
+      end
+
+      it "converts random string to lower-case if necessary" do
+        secret_word = SecretWord.new
+        lowercase_word = secret_word.word.downcase
+        expect(secret_word.word).to eq lowercase_word
       end
     end
   end

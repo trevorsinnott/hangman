51515816b2eb5b08693f872d78702fe57210fecb
Trevor Sinnott <sinnott.trevor@gmail.com>
2018-10-01 09:56:31 +0900

    add check_letter and game_over methods

--- lib/hangman/board.rb
+++ lib/hangman/board.rb
@@ -1,11 +1,42 @@
 module Hangman
   class Board
     # hit?, game_over, winner?, miss_count, word_display
-    attr_accessor :word
+    attr_reader :word
     attr_reader :miss_count
+    attr_reader :reveal
 
-    def initialize
+    def initialize(word = "")
       @miss_count = 0
+      @word = word
+      @reveal = Array.new(@word.length, "_")
+    end
+
+    def check_letter(letter)
+      @word.include?(letter) ? hit : @miss_count += 1
+    end
+
+    def game_over
+      if loser?
+        return :loser
+      elsif winner?
+        return :winner
+      else
+        false
+      end
+    end
+
+    private
+
+    def hit
+      return "hit"
+    end
+
+    def loser?
+      @miss_count < 6 ? false : true
+    end
+
+    def winner?
+      @reveal.join("") == @word ? true : false
     end
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,6 +7,47 @@ module Hangman
         board = Board.new
         expect(board.miss_count).to eq 0
       end
+
+      it "initializes @word to the given string" do
+        board = Board.new("something")
+        expect(board.word).to eq "something"
+      end
+
+      it "intializes @word to empty string if no string given" do
+        board = Board.new
+        expect(board.word).to eq ""
+      end
+
+      it "initializes @reveal to be an array with the same length as @word" do
+        board = Board.new("abc")
+        expect(board.reveal.length).to eq board.word.length
+      end
+
+      it "initializes @reveal as an array of underscores" do
+        board = Board.new("abc")
+        expect(board.reveal).to eq ["_", "_", "_"]
+      end
+    end
+
+    context "#check_letter" do
+      it "increases @miss_count by one if letter is not in @word" do
+        board = Board.new("a")
+        board.check_letter("b")
+        expect(board.miss_count).to eq 1
+      end
+    end
+
+    context "#game_over" do
+      it "returns false if @miss_count is less than 6 and @reveal is not complete" do
+        board = Board.new("something")
+        expect(board.game_over).to eq false
+      end
+
+      it "returns :winner if @reveal is complete" do
+        board = Board.new("abc")
+        allow(board).to receive_messages(reveal: ["a", "b", "c"])
+        expect(board.game_over).to eq :winner
+      end
     end
   end
 end

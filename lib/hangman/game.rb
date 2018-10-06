fb9958d6143ba5ac89b8a1ddef61d03f32270b22
Trevor Sinnott <sinnott.trevor@gmail.com>
2018-10-06 12:59:16 +0900

    setup game class

--- lib/hangman/game.rb
+++ lib/hangman/game.rb
@@ -1,5 +1,29 @@
 module Hangman
   class Game
-    
+    attr_reader :player
+    attr_reader :board
+
+    def initialize(player, board = Board.new)
+      @player = player
+      @board = board
+    end
+
+    def play
+      solicit_guess
+    end
+
+    def solicit_guess
+      puts "#{@player.name}, please guess a letter"
+      get_guess
+    end
+
+    def get_guess(guess = gets.chomp)
+      if guess.length == 1
+        @board.check_letter(guess)
+      else
+        puts "You can't guess that"
+        solicit_guess
+      end
+    end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,6 +1,16 @@
 require "spec_helper"
 module Hangman
   RSpec.describe Game do
-    
+    context "#initialize" do
+      it "initializes @player to provided object" do
+        game = Game.new("John")
+        expect(game.player).to eq "John"
+      end
+
+      it "initializes @board to a new Board object if no argument provided" do
+        game = Game.new("John")
+        expect(game.board.is_a?(Board)).to eq true
+      end
+    end
   end
 end
